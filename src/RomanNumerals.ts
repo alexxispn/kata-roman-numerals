67be32fd2a4932b3d1fecfc53c984ad79d8e99fb
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 15:29:18 +0100

    🔵 refactor: improve implementation to more generic solution

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -1,8 +1,5 @@
 export class RomanNumerals {
-  fromArabic(number: number) {
-    if (number === 3) return "III";
-    if (number === 2) return "II";
-
-    return "I";
+  fromArabic(arabicNumber: number) {
+    return "I".repeat(arabicNumber)
   }
 }
