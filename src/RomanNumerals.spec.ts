f24b1bbbaf645d0c81925bdedb5586ff5c10021a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 15:50:10 +0100

    🔵 refactor: parameterization of X cases

--- src/RomanNumerals.spec.ts
+++ src/RomanNumerals.spec.ts
@@ -2,32 +2,30 @@ import { describe, expect, it } from "vitest";
 import { RomanNumerals } from "./RomanNumerals.js";
 
 describe("Roman Numerals", () => {
-  it.each([
-    [1, "I"],
-    [2, "II"],
-    [3, "III"],
-  ])('converts %i to %s', (arabicNumber, romanNumber) => {
-    const romanNumerals = new RomanNumerals();
-    expect(romanNumerals.fromArabic(arabicNumber)).toBe(romanNumber)
+  describe('I cases', () => {
+    it.each([
+      [1, "I"],
+      [2, "II"],
+      [3, "III"],
+    ])('converts %i to %s', (arabicNumber, romanNumber) => {
+      const romanNumerals = new RomanNumerals();
+      expect(romanNumerals.fromArabic(arabicNumber)).toBe(romanNumber)
+    })
+  })
+
+  describe('X cases', () => {
+    it.each([
+      [10, "X"],
+      [20, "XX"],
+      [30, "XXX"],
+    ])('converts %i to %s', (arabicNumber, romanNumber) => {
+      const romanNumerals = new RomanNumerals();
+      expect(romanNumerals.fromArabic(arabicNumber)).toBe(romanNumber)
+    })
   })
 
   it("converts 5 to V", () => {
     const romanNumerals = new RomanNumerals();
     expect(romanNumerals.fromArabic(5)).toBe("V")
-  })
-
-  it("converts 10 to X", () => {
-    const romanNumerals = new RomanNumerals();
-    expect(romanNumerals.fromArabic(10)).toBe("X")
-  })
-
-  it("converts 20 to XX", () => {
-    const romanNumerals = new RomanNumerals();
-    expect(romanNumerals.fromArabic(20)).toBe("XX")
-  })
-
-  it("converts 30 to XXX", () => {
-    const romanNumerals = new RomanNumerals();
-    expect(romanNumerals.fromArabic(30)).toBe("XXX")
   })
 })
