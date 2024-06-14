6c67bf63df45d3faa17cb1809f9cfba910b09014
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 17:28:08 +0100

    🔵 refactor: extract mapper to static attribute

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -1,20 +1,19 @@
 export class RomanNumerals {
+  static romanArabicConversion = [
+    { roman: "C", arabic: 100 },
+    { roman: "X", arabic: 10 },
+    { roman: "IX", arabic: 9 },
+    { roman: "V", arabic: 5 },
+    { roman: "IV", arabic: 4 },
+    { roman: "I", arabic: 1 }
+  ];
 
   fromArabic(arabicNumber: number) {
-    const romanNumbers = [
-      { roman: "C", arabic: 100 },
-      { roman: "X", arabic: 10 },
-      { roman: "IX", arabic: 9 },
-      { roman: "V", arabic: 5 },
-      { roman: "IV", arabic: 4 },
-      { roman: "I", arabic: 1 }
-    ];
-
     let remaining = arabicNumber;
     let romanNumber = "";
 
     while (remaining > 0) {
-      for (const { roman, arabic } of romanNumbers) {
+      for (const { roman, arabic } of RomanNumerals.romanArabicConversion) {
         if (remaining >= arabic) {
           remaining = remaining - arabic;
           romanNumber += roman;
