cee0695307837a5256f32de2856caae7beb9d199
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 15:40:46 +0100

    🟢 green: converts 10 to X

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -1,6 +1,9 @@
 export class RomanNumerals {
   fromArabic(arabicNumber: number) {
     if(arabicNumber === 5) return 'V'
-    return "I".repeat(arabicNumber)
+
+    if(arabicNumber === 10) return 'X'
+
+    return 'I'.repeat(arabicNumber)
   }
 }
