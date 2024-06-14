50a8272cfb9760788af35b2602fbdf3e9f4b18dd
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 15:32:27 +0100

    🔵 refactor: refactor tests with parameterization

--- src/RomanNumerals.spec.ts
+++ src/RomanNumerals.spec.ts
@@ -2,6 +2,15 @@ import { describe, expect, it } from "vitest";
 import { RomanNumerals } from "./RomanNumerals.js";
 
 describe("Roman Numerals", () => {
+  it.each([
+    [1, "I"],
+    [2, "II"],
+    [3, "III"],
+  ])('converts %i to %s', (arabicNumber, romanNumber) => {
+    let romanNumerals = new RomanNumerals();
+    expect(romanNumerals.fromArabic(arabicNumber)).toBe(romanNumber)
+  })
+
   it("converts 1 to I", () => {
     let romanNumerals = new RomanNumerals();
     expect(romanNumerals.fromArabic(1)).toBe("I");
