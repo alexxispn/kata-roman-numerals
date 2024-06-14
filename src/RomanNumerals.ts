c6bf48ec774214fb4420ddd3bbd87fd87dd8b23c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-14 15:43:01 +0100

    🟢 green: converts 20 to XX

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -3,6 +3,7 @@ export class RomanNumerals {
     if(arabicNumber === 5) return 'V'
 
     if(arabicNumber === 10) return 'X'
+    if(arabicNumber === 20) return 'XX'
 
     return 'I'.repeat(arabicNumber)
   }
